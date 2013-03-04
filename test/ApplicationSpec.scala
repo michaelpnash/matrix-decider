f16281bfb89b1b0d14997343a4d918a4c5fa10fe
Michael Nash <mnash@jglobal.com>
2013-03-04 08:05:30 -0600

    Replace application spec with ScalaTest Freespec equivilant

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,27 +1,26 @@
 package test
 
-import org.specs2.mutable._
-
 import play.api.test._
 import play.api.test.Helpers._
+import org.scalatest.FreeSpec
 
-class ApplicationSpec extends Specification {
-  
-  "Application" should {
-    
-    "send 404 on a bad request" in {
-      running(FakeApplication()) {
-        route(FakeRequest(GET, "/nowhere")) must beNone
+class ApplicationSpec extends FreeSpec {
+
+  "Application" - {
+    "when running" - {
+      "should send 404 on a bad request" in {
+        running(FakeApplication()) {
+          (route(FakeRequest(GET, "/nowhere")) === None)
+        }
       }
-    }
-    
-    "render the index page" in {
-      running(FakeApplication()) {
-        val home = route(FakeRequest(GET, "/")).get
-        
-        status(home) must equalTo(OK)
-        contentType(home) must beSome.which(_ == "text/html")
-        contentAsString(home) must contain ("Matrix Decider")
+      "should render the index page" in {
+        running(FakeApplication()) {
+          val home = route(FakeRequest(GET, "/")).get
+
+          assert(status(home) === OK)
+          assert(contentType(home) === Some("text/html"))
+          assert(contentAsString(home) contains ("Matrix Decider"))
+        }
       }
     }
   }
