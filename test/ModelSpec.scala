35c32ef52b6c0eedccb97d0fbdda37295b2b5876
Michael Nash <mnash@jglobal.com>
2013-03-04 21:00:46 -0600

    Starting on model and corresponding specs

--- test/ModelSpec.scala
+++ test/ModelSpec.scala
@@ -1,13 +1,20 @@
-import model.{Alternative, User, Decision}
+import model._
+import model.Alternative
+import model.Criteria
+import model.Decision
+import model.User
 import org.scalatest.FreeSpec
 
 class ModelSpec extends FreeSpec {
   "a decision" - {
     "can produce a list of alternatives ordered by weighted ranking" in {
-      val alternatives = Set(Alternative("ford", Set()), Alternative("gm", Set()))
+      val price = Criteria("price", 2)
+      val color = Criteria("color", 1)
+      val alternatives = Set(Alternative("ford", Set(Ranking(price, 4))), Alternative("gm", Set()))
       val decision = Decision(User("name"), alternatives, Set())
       val result = decision.alternativesByPreference
       assert(result === alternatives)
     }
+    "can produce a list of criteria order by importance" in (pending)
   }
 }
