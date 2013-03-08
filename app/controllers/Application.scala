d8ff43b78a2ef16aa13121c687d20980f96dd975
Michael Nash <mnash@jglobal.com>
2013-03-08 16:35:45 -0600

    Working towards UI, not there yet

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,12 +7,20 @@ import play.api.data.Forms.{mapping, longNumber, nonEmptyText}
 import play.api.i18n.Messages
 import com.google.inject.{Inject, Singleton}
 
+case class LoginView(login: String)
+
 @Singleton
 class Application @Inject()() extends Controller {
 
+  private val loginForm: Form[LoginView] = Form(
+    mapping(
+      "login" -> nonEmptyText
+    )(LoginView.apply)(LoginView.unapply)
+  )
+
   def index = Action {
     implicit request =>
-    Ok(views.html.index())
+    Ok(views.html.index(loginForm))
   }
 
 }
--- app/controllers/Decisions.scala
+++ app/controllers/Decisions.scala
@@ -1,7 +1,16 @@
 package controllers
 
-import play.api.mvc.Controller
+import play.api._
+import play.api.mvc._
+import play.api.data.Form
+import play.api.data.Forms.nonEmptyText
+import play.api.i18n.Messages
+import com.google.inject.{Inject, Singleton}
+import model.DecisionRepository
 
-class Decisions extends Controller {
-
+@Singleton
+class Decisions @Inject()() extends Controller {
+  def list = Action {
+    Ok("decisions show here")
+  }
 }
