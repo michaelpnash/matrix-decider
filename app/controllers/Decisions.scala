7e213d518d16bc78fdc7eaf8390188b9d561582f
Michael Nash <mnash@jglobal.com>
2013-03-09 15:13:32 -0600

    Reduce logging level, get add alternative operational, make decision edit form a single form, not multiple

--- app/controllers/Decisions.scala
+++ app/controllers/Decisions.scala
@@ -3,45 +3,42 @@ package controllers
 import play.api._
 import play.api.mvc._
 import play.api.data.Form
-import play.api.data.Forms.{mapping, longNumber, nonEmptyText}
+import play.api.data.Forms._
 import play.api.i18n.Messages
 import com.google.inject.{Inject, Singleton}
-import model.{Alternative, UserRepository, Decision, DecisionRepository}
+import model._
 import java.util.UUID
-import model.datastore.AlternativeDataStore
-import scala.slick.session.Database
+import model.Alternative
+import model.Decision
 
-case class DecisionView(name: String)
-case class AlternativeView(name: String)
-case class CriteriaView(name: String)
+case class NewDecisionView(name: String)
+case class DecisionView(alternativeName: Option[String], criteriaName: Option[String])
 
 @Singleton
 class Decisions @Inject()(implicit val decisionRepository: DecisionRepository, userRepository: UserRepository) extends Controller {
 
-   private val decisionForm: Form[DecisionView] = Form(
+  val log = Logger.logger
+
+   private val newDecisionForm: Form[NewDecisionView] = Form(
     mapping(
       "name" -> nonEmptyText
+    )(NewDecisionView.apply)(NewDecisionView.unapply)
+  )
+
+  private val decisionForm: Form[DecisionView] = Form(
+    mapping(
+      "alternativeName" -> optional(text),
+      "criteriaName" -> optional(text)
     )(DecisionView.apply)(DecisionView.unapply)
   )
 
-  private val alternativeForm: Form[AlternativeView] = Form(
-    mapping(
-      "alternativename" -> nonEmptyText
-    )(AlternativeView.apply)(AlternativeView.unapply)
-  )
-
-  private val criteriaForm: Form[CriteriaView] = Form(
-    mapping(
-      "criterianame" -> nonEmptyText
-    )(CriteriaView.apply)(CriteriaView.unapply)
-  )
-
   def list(userId: UUID) = Action { implicit request =>
-    Ok(views.html.decisions(decisionRepository.decisionSpecifiersForUser(userId), decisionForm, userId))
+    Ok(views.html.decisions(decisionRepository.decisionSpecifiersForUser(userId), newDecisionForm, userId))
   }
 
   def newDecision(userId: UUID) = Action { implicit request =>
-     decisionForm.bindFromRequest.fold(
+    log.info("New decision created")
+     newDecisionForm.bindFromRequest.fold(
         formWithErrors => {
           BadRequest(views.html.decisions(decisionRepository.decisionSpecifiersForUser(userId), formWithErrors, userId))
         },
@@ -49,40 +46,31 @@ class Decisions @Inject()(implicit val decisionRepository: DecisionRepository, u
           //save the new decision
           val user = userRepository.findById(userId).get
           decisionRepository.save(Decision(user, Set(), Set(), name = decisionView.name))
-          Ok(views.html.decisions(decisionRepository.decisionSpecifiersForUser(userId), decisionForm, userId))
+          Ok(views.html.decisions(decisionRepository.decisionSpecifiersForUser(userId), newDecisionForm, userId))
         }
       )
   }
 
-  def newAlternative(decisionId: UUID) = Action { implicit request =>
+  def update(decisionId: UUID) = Action { implicit request =>
+    log.info("Decision updating")
     val decision = decisionRepository.findById(decisionId).get
-    alternativeForm.bindFromRequest.fold(
+    decisionForm.bindFromRequest.fold(
         formWithErrors => {
-          BadRequest(views.html.decision(decision, formWithErrors, criteriaForm))
+          BadRequest(views.html.decision(decision, formWithErrors))
         },
-        alternativeView => {
-          //update decision
-          val existing = decisionRepository.findById(decisionId).get
-          decisionRepository.save(existing)
-          Ok(views.html.decision(decision, alternativeForm, criteriaForm))
+        decisionView => {
+          log.info("Update valid:" + decisionView)
+          var newDecision = decisionRepository.findById(decisionId).get
+          if (decisionView.alternativeName.isDefined) newDecision = newDecision.withNewAlternative(Alternative(decisionView.alternativeName.get, Set()))
+          if (decisionView.criteriaName.isDefined) newDecision = newDecision.withNewCriteria(Criteria(decisionView.criteriaName.get, 0))
+          log.info("Updated decision:" + newDecision)
+          Ok(views.html.decision(newDecision, decisionForm))
         }
       )
   }
-
-   def newCriteria(decisionId: UUID) = Action { implicit request =>
-    val decision = decisionRepository.findById(decisionId).get
-    criteriaForm.bindFromRequest.fold(
-        formWithErrors => {
-          BadRequest(views.html.decision(decision, alternativeForm, formWithErrors))
-        },
-        alternativeView => {
-          val newDecision = decisionRepository.findById(decisionId).get.withNewAlternative(Alternative(alternativeView.name, Set()))
-          Ok(views.html.decision(newDecision, alternativeForm, criteriaForm))
-        }
-      )
-  }
+          //val newDecision = decisionRepository.findById(decisionId).get.withNewAlternative(Alternative(alternativeView.name, Set()))
 
   def edit(decisionId: UUID) = Action { implicit request =>
-    Ok(views.html.decision(decisionRepository.findById(decisionId).get, alternativeForm, criteriaForm))
+    Ok(views.html.decision(decisionRepository.findById(decisionId).get, decisionForm))
   }
 }
--- project/Build.scala
+++ project/Build.scala
@@ -10,6 +10,7 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq("com.typesafe.slick" %% "slick" % "1.0.0",
     "org.scalatest" %% "scalatest" % "2.0.M5b" % "test",
     "com.typesafe" %% "scalalogging-slf4j" % "1.0.1"
+
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
