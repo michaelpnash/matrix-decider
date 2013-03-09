4d71e5eb5ea77a66f2f0c0ba121a7e90e9d2cf97
Michael Nash <mnash@jglobal.com>
2013-03-09 10:22:44 -0600

    Working on create new decision mechanism

--- app/controllers/Decisions.scala
+++ app/controllers/Decisions.scala
@@ -9,11 +9,22 @@ import com.google.inject.{Inject, Singleton}
 import model.DecisionRepository
 import java.util.UUID
 
+case class DecisionView(name: String)
+
 @Singleton
 class Decisions @Inject()(decisionRepository: DecisionRepository) extends Controller {
 
+   private val decisionForm: Form[DecisionView] = Form(
+    mapping(
+      "name" -> nonEmptyText
+    )(DecisionView.apply)(DecisionView.unapply)
+  )
+
   def list(userId: UUID) = Action { implicit request =>
-    Ok(views.html.decisions(decisionRepository.decisionSpecifiersForUser(userId)))
+    Ok(views.html.decisions(decisionRepository.decisionSpecifiersForUser(userId), decisionForm))
   }
 
+  def newDecision = Action {
+    Ok("ok")
+  }
 }
