8b0bb6e4afd94764ae9bd138c4a856213870635c
Michael Nash <mnash@jglobal.com>
2013-06-21 09:30:49 -0600

    Fix class name reference for JDBC driver

--- app/infrastructure/Global.scala
+++ app/infrastructure/Global.scala
@@ -19,9 +19,16 @@ object Global extends GlobalSettings {
 class DeciderModule extends AbstractModule {
   val dbSuffix = System.getProperty("db", "mem:testdb")
   val dbUrl = "jdbc:hsqldb:" + dbSuffix
+
+  import com.jolbox.bonecp.BoneCPDataSource
   
+  val ds = new BoneCPDataSource
+  ds.setDriverClass("org.hsqldb.jdbc.JDBCDriver")
+  ds.setJdbcUrl(dbUrl)
+  ds.setAcquireIncrement(5)
+
   override def configure() {
-    bind(classOf[Database]).toInstance(Database.forURL(dbUrl, driver = "org.hsqldb.jdbc.JDBCDriver"))
+    bind(classOf[Database]).toInstance(Database.forDataSource(ds))
     bind(classOf[String]).toInstance("foo")
   }
 }
